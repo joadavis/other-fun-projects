52afa695882b93ccfc1593fa4fbea925526d0b6a
Joseph Davis <joseph.davis@suse.com>
2020-09-02 15:16:24 -0700

    Some work in progress on worst.py
    Found this lying around, decided it should be committed after
    cleaning up a typo.
    Really can't remember where I left off on this.  Need to try
    another run at getting a minimal functionality to parse STL and
    output images.

--- worst3dprinterever-v2/worst.py
+++ worst3dprinterever-v2/worst.py
@@ -74,6 +74,9 @@ class STL_Shape(object):
         self.triangles = []  # triangles can be a tuple with ((normal), (p1), (p2), (p3), attrib)
         self.min_point = [1000.0, 1000.0, 1000.0]   # hoping these are far enough out
         self.max_point = [-1000.0, -1000.0, -1000.0]
+        self.scaling_ratio = 1
+        self.transcale_triangles = []  # triangles that have been scaled and shifted into the "print area"
+        self.sheet_images = []  # one image per sheet of paper
 
     def update_minmax(self, point):
         for coord in range(3):
@@ -81,6 +84,36 @@ class STL_Shape(object):
                 self.min_point[coord] = point[coord]
             if point[coord] > self.max_point[coord]:
                 self.max_point[coord] = point[coord]
+
+    def calc_scaling_ratio(self, print_max_point):
+        """Given a maximum point from 0 to contain the print,
+        calculate a scaling ratio for use on all 3 dimensions
+        max_point: distance from 0,0,0 that the print area
+        contains
+        :returns single float to use in scaling
+        """
+        ratios = []
+        for dim in range(3):
+            ratios.append(print_max_point[dim] / (self.max_point[dim] - self.min_point[dim]))
+            if verbose > 0:
+                print("limit {} / ({} - {}) gives ratio {}".format(
+                    print_max_point[dim], self.max_point[dim], self.min_point[dim], ratios[dim]))
+        # pick the smallest ratio so it won't overflow in any direction
+        if ratios[0] < ratios[1] and ratios[0] < ratios[2]:
+            self.scaling_ratio = ratios[0]
+            return ratios[0]
+        elif ratios[1] < ratios[2]:
+            self.scaling_ratio = ratios[1]
+            return ratios[1]
+        else:
+            self.scaling_ratio = ratios[2]
+            return ratios[2]
+
+    def render_triangles_to_images(self):
+        if len(self.triangles) == 0:
+            print("No triangles, can't render")
+            raise Exception("no triangles to render")
+        # create blank images
 
     def prettyprint(self):
         print("header: {}".format(self.header))
@@ -127,18 +160,21 @@ def read_stl_bin(filepath):
 if __name__ == '__main__':
     # some defaults
     infile = "/home/joadavis/personal/model data/tinkercad/itsa_me.stl"
-    #out_dir = "./worst-run/"
+    out_dir = "./worst-run/"
     sheets_needed = 250  # default to an inch. a typical 20lb ream (500 sheets) of paper is 2 inches thick
     sheet_size_x = 10.5  # 11 inches minus a print margin
     sheet_size_z = 8     # 8.5 inches minus a print margin
+    sheet_resolution = 250  # note 300dpi is a common printer resolution.  use this value for x and z
     verbose = 0
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbosity", action="count", default=0)
     parser.add_argument("-i", "--in_file", type=str, help="The path to an input file (binary STL only)")
-    parser.add_argument("-o", "--out_dir", type=str, help="output directory, default is input name less extension")
+    parser.add_argument("-o", "--out_dir", type=str, help="output directory, "
+                                                          "default is input name less extension")
     parser.add_argument("-p", "--pages_per_inch", type=int, default=250,
-                        help="Number of pages stacked up to make an inch, related to the weight of the paper used,"
+                        help="Number of pages stacked up to make an inch, related to the weight "
+                             "of the paper used,"
                              " default is 250")
     parser.add_argument("-x", "--desired_x", type=float, default=1.0,
                         help="desired x width in inches, default is 1 inch")
@@ -156,13 +192,28 @@ if __name__ == '__main__':
         sheets_needed += 1
     print("Expect this to need {} sheets of paper with {} per sheet".format(sheets_needed, images_per_sheet))
 
+    printarea_max_point = (args.desired_x * sheet_resolution,
+                           sheets_needed * images_per_sheet, args.desired_y * sheet_resolution)
+    print("Print area max point is {}".format(printarea_max_point))
+    # something seems a little off here, but go for it
+
     if args.in_file:
         # TODO sanity checking of path
         infile = args.in_file
     # TODO check on outfile, default value, create path if needed
+    if args.out_dir:
+        out_dir = args.out_dir
     verbose = args.verbosity
 
     # temp - do something
     shape = read_stl_bin(infile)
     if verbose > 2:
         shape.prettyprint()
+
+    # decide how the infile will fit in the print area
+    print("winning scaling factor is {}".format(shape.calc_scaling_ratio(printarea_max_point)))
+
+    shape.render_triangles_to_images()
+
+    # last thing
+    print("End run.")
