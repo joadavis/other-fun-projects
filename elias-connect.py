dbed403480f2e9cb709999b6b62b4a54d54a0e4e
Joseph Davis <jodavis@suse.com>
2020-10-15 17:34:07 -0700

    elias-connect.py: add bomb powerup

--- elias-connect.py
+++ elias-connect.py
@@ -29,10 +29,10 @@ class GameBoard(object):
     The game is a basic 7 columns and 6 rows grid where pieces are dropped one by one from the top.
     Players take turns selecting an available column to drop in a piece of their color.
     However, there are power ups taht can be enabled to make the game more chaotic.
-    - Bombs can be dropped instead of a piece and will destroy pieces from a small area.
-    - Shields can be placed over a column to block more pieces being dropped for a number of turns.
-    - A shock can be dropped in a column so when the opponent drops a piece they get shocked.  3 shocks and a player loses a turn.
-    - Anti gravity - all the columns shift up in the grid, and play continues where pieces are inserted from the bottom.
+    - Bombs can be dropped instead of a piece and will destroy pieces from a small area where it hits - one below, one right, one left.
+    - (TODO) Shields can be placed over a column to block more pieces being dropped for a number of turns.
+    - (TODO) A shock can be dropped in a column so when the opponent drops a piece they get shocked.  3 shocks and a player loses a turn.
+    - (TODO) Anti gravity - all the columns shift up in the grid, and play continues where pieces are inserted from the bottom.
     There may be future enhancements to add random powerups and pickups.
     """
 
@@ -61,6 +61,44 @@ class GameBoard(object):
                 self.board[col][rown - 1] = chip
                 return
         self.board[col][self.num_rows - 1] = chip
+
+    def drop_bomb(self, col):
+        """ Drop a bomb in to a column.
+            When it contacts a chip, remove that chip and surrounding chips (left right below)
+            If it hits the bottom, just remove left and right """
+        # TODO create a booom board to show X where the bombs went off before downward shuffling
+        for rown in range(0, self.num_rows - 1):
+            if self.board[col][rown] != 0:
+                # blow up by replacing with 0
+                self.board[col][rown] = 0
+                if rown < self.num_rows:
+                    self.board[col][rown +1] = 0
+                if col > 0:
+                    self.board[col-1][rown] = 0
+                    self.apply_gravity_to_col(col-1)
+                if col < self.num_columns:
+                    self.board[col+1][rown] = 0
+                    self.apply_gravity_to_col(col+1)
+                return
+        # hit the bottom, so just try neighbors
+        if self.board[col][self.num_rows-1] != 0:
+            self.board[col][self.num_rows-1] = 0
+        if col > 0:
+            self.board[col-1][self.num_rows-1] = 0
+            self.apply_gravity_to_col(col-1)
+        if col < self.num_columns:
+            self.board[col+1][self.num_rows-1] = 0
+            self.apply_gravity_to_col(col+1)
+
+
+    def apply_gravity_to_col(self, col):
+        """ Keep it simple, just assume only one gap.  Apply again if more."""
+        for rown in range(self.num_rows-1, 1, -1):
+            if self.board[col][rown] == 0:
+                # swap
+                self.board[col][rown] = self.board[col][rown-1]
+                self.board[col][rown-1] = 0
+
 
     def check_for_winner(self):
         """ horiz, vert, diag from top left, other diag"""
@@ -228,6 +266,10 @@ def test_case_diag_ltd():
         print("Test failed! should have found a winner")
 
 
+def parse_column_input():
+    #todo refactor to here
+    pass
+
 # return action chosen, validated column to drop
 def parse_input(avail_cols):
     # TODO allow input like "d3"
@@ -236,12 +278,23 @@ def parse_input(avail_cols):
     if act == None or act == "":
         return None, None
     if act.startswith("d"):
+        if act[1:].isdigit() and int(act[1:]) in avail_cols:
+            return "d", int(act[1:])
         # TODO just convert the input to a number and call a function
         act = input("What column do you choose? ")
         if act == None or act == "":
             return None, None
         if act.isdigit() and int(act) in avail_cols:
             return "d", int(act)
+    elif act.startswith("p"):
+        if act[1:].isdigit() and int(act[1:]) < GameBoard.num_columns:
+            return "p", int(act[1:])
+        print("The only powerup is a bomb!")
+        act = input(f"What column do you choose? 0-{GameBoard.num_columns} ")
+        if act == None or act == "":
+            return None, None
+        if act.isdigit() and int(act) < GameBoard.num_columns:
+            return "p", int(act)
 
     # invalid input, try again
     return None, None
@@ -259,6 +312,10 @@ def player_turn(gb, player_number):
             gb.drop_in(col, player_number)
             need_input = False
         # TODO: handle powerups
+        elif act == "p":
+            print("BOOOM")
+            gb.drop_bomb(col)
+            need_input = False
         else:
             print("Invalid input or not implemented yet, please try again.")
         # if act == None, fall through and loop again
@@ -268,29 +325,16 @@ def game_loop(shields=False, bombs=False, shocks=False):
     noone_has_won = True
     # TODO get user names?
     
-    while noone_has_won:
-        # TODO show valid columns
-        gb.display()
-        avail_cols = gb.list_open_columns()
-        print(f"  These columns are available {avail_cols}")
-        print(f">> Player {1} turn <<")
-        # TODO get input for player 1
-        need_input = True
-        while need_input:
-            act, col = parse_input(avail_cols)
-            if act == "d":
-                gb.drop_in(col, 1)
-                need_input = False
-            # TODO: handle powerups
-            else:
-                print("Invalid input or not implemented yet, please try again.")
-            # if act == None, fall through and loop again
+    while noone_has_won:        
+        player_turn(gb, 1)
 
         # check for winner
         checked = gb.check_for_winner()
         if checked != None:
+            gb.display()
             print(checked)
             print("Thanks for playing!")
+            print()
             break
 
         # repeat for player 2
@@ -298,15 +342,17 @@ def game_loop(shields=False, bombs=False, shocks=False):
         # check for winner
         checked = gb.check_for_winner()
         if checked != None:
+            gb.display()
             print(checked)
             print("Thanks for playing!")
+            print()
             break
 
 
 if __name__ == '__main__':
 
     print("Welcome to Elias' Connect Game")
-    act = input("Do you want to play with (y) or without (n) powerups? Or type h for help. ")
+    act = input("Do you want to play with (y or enter) or without (n) powerups? Or type h for help. ")
 
     # Secret run tests mode
     if act.startswith("t"):
